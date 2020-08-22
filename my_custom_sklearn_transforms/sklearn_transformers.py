579e9750422941cad0117b5dcc66e48451e998dd
MarcosSoares10 <marquinhoscamaleao@gmail.com>
2020-08-22 15:40:16 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import resample
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +15,55 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+ 
+# Retorna um novo dataframe com valor nulo igual a média dos demais values nas colunas escolhidas
+class RecalculateNullNumericValues(BaseEstimator, TransformerMixin):
+    def fit(self,X,y=None):
+        return self
+
+    def transform(self, X,InitialNumericColumn,FinalNumericColumn):
+        data = X.copy()
+        values = data.loc[:,InitialNumericColumn:FinalNumericColumn]
+        values = values.T.fillna(round(values.mean(axis=1),1)).T
+        data.loc[:,InitialNumericColumn:FinalNumericColumn] = values
+        return data
+
+# Retorna um novo dataframe com dados balanceados, para isso é verificado se a melhor opção é o Oversampling ou Downsampling
+class BalanceClasses():
+    def oversample(self, classe, n_samples):
+        return resample(classe, replace=True, n_samples=n_samples, random_state=42)
+        
+    def undersample(self, classe, n_samples):
+        return resample(classe, replace=False, n_samples=n_samples, random_state=42 )
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        target = []
+        freq = data.PERFIL.value_counts()
+        mean_avg = int( freq.mean() )
+        resampled = []
+        
+        for value in data.PERFIL.unique():
+            target.append( data[data.PERFIL == value] )
+            if target[-1].shape[0] > mean_avg:
+                resampled.append( self.undersample( target[-1], mean_avg ))
+            else:
+                resampled.append( self.oversample( target[-1], mean_avg ))
+        
+        return pd.concat(resampled)
+
+    
+# Transformação Clamp para garantir valores de notas entre 0-10
+class ClampClasses():
+    def fit(self, X, y=None):
+          return self
+
+    def transform(self, X,InitialNumericColumn,FinalNumericColumn=None):
+          data = X.copy()
+          values = data.loc[:,InitialNumericColumn:FinalNumericColumn]
+          values = values.clip(0.0,10.0)
+          data.loc[:,InitialNumericColumn:FinalNumericColumn] = values
+          return data
