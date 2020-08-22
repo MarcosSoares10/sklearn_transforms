cba14448e493ad964ad36c37581cd8785cd1bb8b
MarcosSoares10 <marquinhoscamaleao@gmail.com>
2020-08-22 15:36:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/MarcosSoares10/sklearn_transforms/',
+      author='Marcos Oliveira',
+      author_email='oliveiramarcos.dev@gmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
